3e7a40644c15bb7f94c85ec84c6b78a1a4193fbb
AbdulRehman <likeam99@gmail.com>
2022-05-31 21:54:10 +0500

    ok

--- app.js
+++ app.js
@@ -11,8 +11,51 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout{
 
-let map, mapEvent;
+    date = new Date();
+    id = (new Date() + '').slice(-10);
+
+    constructor(coords, distance, duration){
+        // this.date = ...
+        // this.id = ...
+        this.coords = coords;
+        this.distance = distance;
+        this.duration = duration;
+    }
+}
+
+class Running extends Workout{
+    constructor(coords, distance, duration, candence){
+        super(coords, distance, duration);
+        this.candence = candence;
+        this.calcPace(); 
+    }
+
+    calcPace(){
+
+        this.pace = this.duration / this.distance;
+        return this.pace;
+    }
+}
+
+class Cycling extends Workout{
+    constructor(coords, distance, duration, elevationGain){
+        super(coords, distance, duration);
+        this.elevationGain = elevationGain;
+        this.calcSpeed();
+    }
+
+    calcSpeed(){
+
+        this.speed = this.distance / (this.duration / 60);
+        return this.speed;
+    }
+}
+
+const run1 = new Running([39, -12], 5.2, 24, 178);
+const cycling1 = new Cycling([39, -12], 27, 95, 523);
+console.log(run1, cycling1);
 
 
 
